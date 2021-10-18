30bf5fbb4c01035e83a0bb22bedd663d5a4c3e15
080812 <74765678+080812@users.noreply.github.com>
2021-10-18 15:37:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+LWX=0;
+RWX=0;
+NoseX=0;
+NoseY=0;
+distance=0;
+
+function preload(){
+
+}
+
+function setup(){
+  canvas=createCanvas(500,500)  
+  canvas.center()
+  video=createCapture(VIDEO)
+  video.size(500,500)
+
+  poseNet=ml5.poseNet(video,modelLoaded)
+  poseNet.on('pose',gotposes)
+}
+
+function draw(){
+background('#000080')
+square(NoseX,NoseY,distance)
+}
+
+function modelLoaded(){
+  console.log('model is loaded')
+}
+
+function gotposes(results){
+  if(results.length>0){
+    console.log(results);
+
+    NoseX=results[0].pose.nose.x;
+    NoseY=results[0].pose.nose.y;
+
+    LWX=results[0].pose.leftWrist.x;
+    RWX=results[0].pose.rightWrist.x;
+    distance=Math.floor(LWX-RWX);
+console.log(distance);
+document.getElementById("square_sides").innerHTML="Side Of The Square " + distance
+
+  }
+}
